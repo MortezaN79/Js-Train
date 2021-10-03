06d81aeab313d7c4adda90da3af602f916b23ecd
MortezaN79 <89310696+MortezaN79@users.noreply.github.com>
2021-10-03 14:52:12 +0330

    Create Min&Max.js

---
+++ Min&Max.js
@@ -0,0 +1,15 @@
+function min(a,b) {
+	 return a>b? b:a
+}
+
+console.log(min(6,3))
+console.log(min(1,7))
+
+
+function max (a,b){
+	return a>b? a:b
+}
+
+
+console.log(max(6,3))
+console.log(max(1,7))
